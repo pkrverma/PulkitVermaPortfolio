f566ee301682f01539cdbdf78e2e5359a77bfc77
Pulkit Verma <pulkit18012003@gmail.com>
2024-02-18 03:17:50 +0530

    navbar icon added

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,41 @@
-consoleText(['Frontend Developer', 'Content Writer', 'UI/UX Designer'], 'text',['White']);
+// Navbar button
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function yourFunction() {
+  const menuBtn = document.getElementById('myNavBar');
+
+  if (window.innerWidth <= 720) {
+    menuBtn.innerHTML = '<div class="title"><h2><a href="#banner" aria-current="page">Pulkit Kumar Verma</a></h2></div><div class="nav-tab sidebar" id="mySidebar"><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a><ul><li><a href="#about">About</a></li><li><a href="#skill">Skills</a></li><li><a href="#project">Projects</a></li><li><a href="#contact">Contact</a></li></ul></div><div id="main"><button class="openbtn " onclick="openNav()"><i class="fa-solid fa-bars"></i></button></div>';
+  } else {
+    menuBtn.innerHTML = '<div class="title"><h2><a href="#banner" aria-current="page">Pulkit Kumar Verma</a></h2></div><div class="nav-tab sidebar" id="mySidebar"><ul><li><a href="#about">About</a></li><li><a href="#skill">Skills</a></li><li><a href="#project">Projects</a></li><li><a href="#contact">Contact</a></li></ul></div>';
+  }
+}
+
+window.onload = yourFunction;
+window.onresize = yourFunction;
+
+// const main = document.getElementById('mySidebar');
+
+
+// if (window.innerWidth <= 720) {
+//   main.innerHTML = '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a><div id="main"><button class="openbtn " onclick="openNav()"><i class="fa-solid fa-bars"></i></button></div>';
+// }
+// else {
+//   main.innerHTML = '';
+// }
+
+
+// banner text animatiom
+
+consoleText(['Frontend Developer', 'Content Writer', 'UI/UX Designer'], 'text', ['White']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
@@ -9,12 +46,12 @@ function consoleText(words, id, colors) {
   var waiting = false;
   var target = document.getElementById(id)
   target.setAttribute('style', 'color:' + colors[0])
-  window.setInterval(function() {
+  window.setInterval(function () {
 
     if (letterCount === 0 && waiting === false) {
       waiting = true;
       target.innerHTML = words[0].substring(0, letterCount)
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         var usedColor = colors.shift();
         colors.push(usedColor);
         var usedWord = words.shift();
@@ -26,7 +63,7 @@ function consoleText(words, id, colors) {
       }, 1000)
     } else if (letterCount === words[0].length + 1 && waiting === false) {
       waiting = true;
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         x = -1;
         letterCount += x;
         waiting = false;
@@ -36,7 +73,7 @@ function consoleText(words, id, colors) {
       letterCount += x;
     }
   }, 120)
-  window.setInterval(function() {
+  window.setInterval(function () {
     if (visible === true) {
       con.className = 'console-underscore hidden'
       visible = false;
